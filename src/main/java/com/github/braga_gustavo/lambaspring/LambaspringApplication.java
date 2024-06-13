c7e6c1ad3855084b33c754b8e32f51d9ccd28655
braga-gustavo <gustavo.b.moraes2@gmail.com>
2024-06-13 16:18:36 -0300

    Api integration e derealisation

--- src/main/java/com/github/braga_gustavo/lambaspring/LambaspringApplication.java
+++ src/main/java/com/github/braga_gustavo/lambaspring/LambaspringApplication.java
@@ -1,5 +1,8 @@
 package com.github.braga_gustavo.lambaspring;
 
+import com.github.braga_gustavo.lambaspring.model.DataConverter;
+import com.github.braga_gustavo.lambaspring.model.SeriesData;
+import com.github.braga_gustavo.lambaspring.service.MovieApiIntegration;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +10,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LambaspringApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LambaspringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LambaspringApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Hello World");
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        MovieApiIntegration apiService = new MovieApiIntegration();
+        var json = apiService.getData("https://www.omdbapi.com/?t=breaking+bad&apikey=2b301e2f");
+        System.out.println(json);
+        DataConverter converter = new DataConverter();
+        SeriesData data = converter.getData(json, SeriesData.class);
+        System.out.println(data);
+
+
+    }
 }
